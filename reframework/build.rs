cd85fe9d131cc469c4a927665e991a4d0fd5b13e
Kvasszn <kvasszn@gmail.com>
2026-06-04 00:25:54 -0600

    AI deslop and remove imgui examples

--- reframework/build.rs
+++ reframework/build.rs
@@ -15,34 +15,4 @@ fn main() {
     bindings
         .write_to_file(out_path.join("reframework_bindings.rs"))
         .expect("Couldn't write bindings!");
-
-    /*let bindings = bindgen::Builder::default()
-        .header("include/imgui.h")
-        .clang_arg("-x")
-        .clang_arg("c++")
-        .clang_arg("-std=c++17")
-        .clang_arg("-DIMGUI_USE_WCHAR32")
-        .clang_arg("-DIMGUI_ENABLE_FREETYPE")
-        .clang_arg("-DIMGUI_IMPL_WIN32_DISABLE_GAMEPAD")
-        .clang_arg("-DIM_ASSERT(x)=((void)(x))")
-        .layout_tests(false)
-        .generate()
-        .expect("Unable to generate ImGui bindings");
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-        bindings
-            .write_to_file(out_path.join("imgui_bindings.rs"))
-            .expect("Couldn't write bindings!");
-
-    cc::Build::new()
-        .cpp(true)
-        .file("third-party/imgui/imgui.cpp")
-        .file("third-party/imgui/imgui_draw.cpp")
-        .file("third-party/imgui/imgui_widgets.cpp")
-        .file("third-party/imgui/imgui_tables.cpp")
-        .define("IMGUI_USER_CONFIG", "\"../../../include/imconfig.h\"")
-        .define("IMGUI_DISABLE_DEFAULT_ALLOCATORS", None)
-        .flag("-std=c++17")
-        .warnings(false)
-        .compile("imgui");*/
 }
--- reframework/imgui-sys/build.rs
+++ reframework/imgui-sys/build.rs
@@ -11,9 +11,7 @@ fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let mut freetype_includes = Vec::new();
 
-    // 1. Resolve FreeType based on the target platform
     if is_windows {
-        // Point directly to your vendored Windows MSVC folder
         let freetype_lib_dir = format!("{}/thirdparty/freetype/lib", manifest_dir);
         let freetype_include_dir = format!("{}/thirdparty/freetype/include", manifest_dir);
 
@@ -22,7 +20,6 @@ fn main() {
 
         freetype_includes.push(PathBuf::from(freetype_include_dir));
     } else {
-        // Fallback for native Linux development
         let freetype = pkg_config::Config::new()
             .atleast_version("2.0")
             .probe("freetype2")
@@ -31,14 +28,12 @@ fn main() {
         freetype_includes = freetype.include_paths;
     }
 
-    // 2. Configure Include Paths for the C++ Compiler
     build.include("thirdparty/imgui");
     build.include("thirdparty/cimgui");
     for path in &freetype_includes {
         build.include(path);
     }
 
-    // 3. Compile the Vendored C++ Code
     let files = [
         "thirdparty/imgui/imgui.cpp",
         "thirdparty/imgui/imgui_draw.cpp",
@@ -46,7 +41,7 @@ fn main() {
         "thirdparty/imgui/imgui_widgets.cpp",
         "thirdparty/imgui/misc/freetype/imgui_freetype.cpp",
         "thirdparty/cimgui/cimgui.cpp",
-        "thirdparty/imgui/imgui_demo.cpp", // <-- ADD THIS LINE
+        "thirdparty/imgui/imgui_demo.cpp",
     ];
 
     for file in &files {
@@ -55,27 +50,22 @@ fn main() {
 
     build.define("IMGUI_ENABLE_FREETYPE", None);
     build.define("IMGUI_ENABLE_STB_TRUETYPE", "1");
-    // Inject REFramework's custom config
     build.define("IMGUI_USER_CONFIG", "\"re2_imconfig.hpp\""); 
     
     build.compile("cimgui");
 
-    // 4. Generate the Rust Bindings
     println!("cargo:rerun-if-changed=wrapper.h");
     
     let mut builder = bindgen::Builder::default()
         .header("wrapper.h")
         .clang_arg(format!("--target={}", target))
-        // Tell bindgen where the vendored folders are
         .clang_arg("-Ithirdparty/cimgui")
         .clang_arg("-Ithirdparty/imgui")
         .clang_arg("-DIMGUI_ENABLE_FREETYPE")
         .clang_arg("-DIMGUI_ENABLE_STB_TRUETYPE=1")
-        // Tell bindgen to read the custom config
         .clang_arg("-DIMGUI_USER_CONFIG=\"re2_imconfig.hpp\"")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()));
 
-    // Feed the FreeType include paths to bindgen
     for path in &freetype_includes {
         builder = builder.clang_arg(format!("-I{}", path.display()));
     }
@@ -84,7 +74,6 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
-    // 5. Write the generated bindings to the output directory
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
